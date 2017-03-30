d1a2abf4632266106bacfb8340953a4efe0e0a94
Paweł Dudziński <pawel.d@webinterpret.com>
2017-03-30 12:20:55 +0200

    Text formatting

--- src/kvstorage_server.erl
+++ src/kvstorage_server.erl
@@ -7,19 +7,19 @@
 %% ------------------------------------------------------------------
 
 -export([start_link/0,
-		 kv_get/1,
-		 kv_post/2,
-		 kv_put/2,
-		 kv_delete/1]).
+         kv_get/1,
+         kv_post/2,
+         kv_put/2,
+         kv_delete/1]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
 %% ------------------------------------------------------------------
 
 -export([init/1,
-		 handle_call/3,
-		 handle_cast/2,
-		 handle_info/2,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
          terminate/2,
          code_change/3]).
 
@@ -31,21 +31,21 @@ start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
     
 kv_get(Key) ->
-	Result = gen_server:call(?SERVER, {rest, get, Key}),
-	{ok, Result}.
+    Result = gen_server:call(?SERVER, {rest, get, Key}),
+    {ok, Result}.
 
 kv_delete(Key) ->
-	Result = gen_server:call(?SERVER, {rest, delete, Key}),
-	{ok, Result}.
+    Result = gen_server:call(?SERVER, {rest, delete, Key}),
+    {ok, Result}.
 
 kv_post(Key, Value) ->
-	Result = gen_server:call(?SERVER, {rest, post, Key, Value}),
-	{ok, Result}.
-	
+    Result = gen_server:call(?SERVER, {rest, post, Key, Value}),
+    {ok, Result}.
+    
 kv_put(Key, Value) ->
-	Result = gen_server:call(?SERVER, {rest, put, Key, Value}),
-	{ok, Result}.
-	
+    Result = gen_server:call(?SERVER, {rest, put, Key, Value}),
+    {ok, Result}.
+    
 
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
@@ -55,19 +55,19 @@ init([]) ->
     {ok, ets:new(store, [set, named_table])}.
 
 handle_call({rest, get, Key}, _From, Store) ->
-	Result = kvstorage_storage:storage_get(Key, Store),
+    Result = kvstorage_storage:storage_get(Key, Store),
     {reply, Result, Store};
 
 handle_call({rest, post, Key, Value}, _From, Store) ->
-	Result = kvstorage_storage:storage_save(Key, Value, Store),
+    Result = kvstorage_storage:storage_save(Key, Value, Store),
     {reply, Result, Store};
 
 handle_call({rest, put, Key, Value}, _From, Store) ->
-	Result = kvstorage_storage:storage_update(Key, Value, Store),
+    Result = kvstorage_storage:storage_update(Key, Value, Store),
     {reply, Result, Store};
 
 handle_call({rest, delete, Key}, _From, Store) ->
-	Result = kvstorage_storage:storage_delete(Key, Store),
+    Result = kvstorage_storage:storage_delete(Key, Store),
     {reply, Result, Store};
   
 handle_call(_Request, _From, State) ->
