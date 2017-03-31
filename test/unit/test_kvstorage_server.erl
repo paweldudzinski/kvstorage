0082d5886394923d9d94591db78853fde55a805e
Paweł Dudziński <pawel.d@webinterpret.com>
2017-03-31 14:59:04 +0200

    Update tests

--- test/unit/test_kvstorage_server.erl
+++ test/unit/test_kvstorage_server.erl
@@ -25,33 +25,33 @@ stop(TestStore) ->
 
 test_handle_call_get(TestStore) ->
 	NotFoundResult = kvstorage_server:handle_call(
-		{rest, get, "key"}, self(), TestStore),
+		{get, "key"}, self(), TestStore),
 	ets:insert(TestStore, {"key", "value"}),
 	FoundResult = kvstorage_server:handle_call(
-		{rest, get, "key"}, self(), TestStore),
+		{get, "key"}, self(), TestStore),
 	[?_assertEqual(NotFoundResult, {reply, not_found, store}),
 	 ?_assertEqual(FoundResult, {reply, {found,"value"}, store})].
 
 test_handle_call_post(TestStore) ->
 	SaveResult = kvstorage_server:handle_call(
-		{rest, post, "key", "value"}, self(), TestStore),
+		{post, "key", "value"}, self(), TestStore),
 	[?_assertEqual(SaveResult, {reply, created, store})].
 
 test_handle_call_put(TestStore) ->
 	NotFoundResult = kvstorage_server:handle_call(
-		{rest, put, "key", "x"}, self(), TestStore),
+		{put, "key", "x"}, self(), TestStore),
 	ets:insert(TestStore, {"key", "value"}),
 	UpdateResult = kvstorage_server:handle_call(
-		{rest, put, "key", "x"}, self(), TestStore),
+		{put, "key", "x"}, self(), TestStore),
 	[?_assertEqual(NotFoundResult, {reply, not_found, store}),
 	 ?_assertEqual(UpdateResult, {reply, updated, store})].
 
 test_handle_call_delete(TestStore) ->
 	NotFoundResult = kvstorage_server:handle_call(
-		{rest, delete, "key"}, self(), TestStore),
+		{delete, "key"}, self(), TestStore),
 	ets:insert(TestStore, {"key", "value"}),
 	DeletedResult = kvstorage_server:handle_call(
-		{rest, delete, "key"}, self(), TestStore),
+		{delete, "key"}, self(), TestStore),
 	[?_assertEqual(NotFoundResult, {reply, not_found, store}),
 	 ?_assertEqual(DeletedResult, {reply, deleted, store})].
 
